163a4aa74b2b666f155c689fcb1e0f29f497ed7c
Lucas Gabriel <gabif.johann@gmail.com>
2022-10-26 00:32:38 -0300

    iterando no jsx

--- fundamentos-reactjs/src/App.jsx
+++ fundamentos-reactjs/src/App.jsx
@@ -3,24 +3,59 @@ import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
+
 import './global.css';
 
-function App() {
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋', },
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉&nbsp;&nbsp;jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋', },
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉&nbsp;&nbsp;jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-26 20:00:00'),
+  }
+];
+
+// iteração é sempre feita com map no JSX
+export function App() {
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Diego Fernandes"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          />
-          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+         {post.map(post => {
+          return (
+            <Post 
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt}
+            />
+          )
+         })}
         </main>
       </div>
     </div>
   );
 }
-
-export default App;
